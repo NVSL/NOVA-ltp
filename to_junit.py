87866e003f60c9003067f998a07db67fe366f886
Steven Swanson <swanson@cs.ucsd.edu>
2017-06-11 05:35:09 +0000

    More robust outcome handling

--- to_junit.py
+++ to_junit.py
@@ -12,10 +12,12 @@ def skipped(name):
     return ""
 
 def success(name):
+    print name
     a = re.split('\s+',name,maxsplit=2)
     return '<testcase classname="{}" name="{}"/>'.format(a[0], a[1])
 
 def failure(name, kind, reason):
+    print name
     a = re.split('\s+',name,maxsplit=2)
     return """<testcase classname="{}" name="{}">
         <failure type="{}"> {}</failure>
@@ -25,13 +27,13 @@ def failure(name, kind, reason):
 out = []
 
 while l < max_l:
-    g = re.search("(^.*)T(PASS|FAIL)(.*)", lines[l])
+    g = re.search("(^.*\d+)\s+T([A-Z]+)(.*)", lines[l])
     if g:
         print g.group(2)
         if g.group(2) == "PASS":
             out.append(success(g.group(1)))
-        elif g.group(2) == "FAIL":
-            out.append(failure(g.group(1), "failed", g.group(3)))
+        else:
+            out.append(failure(g.group(1), g.group(2), g.group(3)))
             
     l += 1
 
